68f1dc7f3a5d521348586ae921d4172ac89de80e
Andrea Talbot <andrea.talbot1@gmail.com>
2014-10-07 12:32:45 -0500

    password reset logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 FeyaCandle::Application.routes.draw do
 
+  get "password_resets/create"
+
+  get "password_resets/edit"
+
+  get "password_resets/update"
+
   resources :locations, :users, :products
   
   get 'home' => 'pages#home', :as => 'home'
