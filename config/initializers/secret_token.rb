6abbb357d2adbcc4290f2f1323106a8c5f6a9005
Andrea Talbot <andrea.talbot1@gmail.com>
2014-10-07 10:38:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeyaCandle::Application.config.secret_token = 'dcfd16df1658a663d16d55978f64c700479fa7332bd27b8bf48a0636738cc0357cbe8613819b280c244a45187005d03437566a0287f014001473fa485340601a'
