f16f6bb08d0578ca268e666ec420fbf310fdb9b5
Andrea Talbot <andrea.talbot1@gmail.com>
2014-10-07 12:03:59 -0500

    sorcery set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  attr_accessible :crypted_password, :email, :salt
+  authenticates_with_sorcery!
+  
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+  
+  validates :email, uniqueness: true
+  
 end
