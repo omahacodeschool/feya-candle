530bc142971511fd1b2997ad31bda5cc0d93d2a8
Andrea Talbot <andrea.talbot1@gmail.com>
2014-10-07 11:02:51 -0500

    products model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20141007155318) do
+
+  create_table "products", :force => true do |t|
+    t.string   "name"
+    t.string   "price"
+    t.string   "image"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
 end
