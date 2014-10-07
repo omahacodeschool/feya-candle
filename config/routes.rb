ef08f543ef775f44a0c5e4c2ac26273b9631411c
Andrea Talbot <andrea.talbot1@gmail.com>
2014-10-07 13:35:34 -0500

    added flash messaging to application layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 FeyaCandle::Application.routes.draw do
 
-  # get "user_sessions/new"
-  #
-  # get "user_sessions/create"
-  #
-  # get "user_sessions/destroy"
-
   get "password_resets/create"
   get "password_resets/edit"
   get "password_resets/update"
