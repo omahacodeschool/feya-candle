1772389ec53b404c82402037f4cadddfcb9d4a5b
Andrea Talbot <andrea.talbot1@gmail.com>
2014-10-07 15:14:34 -0500

    set heroku env variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,8 +15,11 @@ FeyaCandle::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => "dev.feya-candle.herokuapp.com" }
-
+  config.action_mailer.default_url_options = { 
+    :host => "dev.feya-candle.herokuapp.com",
+    :protocol => "https"
+  }
+  
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,7 +28,12 @@ FeyaCandle::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-    config.action_mailer.default_url_options = { :host => "test.feya-candle.herokuapp.com" }
+  
+
+  config.action_mailer.default_url_options = { 
+    :host => "test.feya-candle.herokuapp.com",
+    :protocol => "https"
+  }
 
   # Raise exception on mass assignment protection for Active Record models
   config.active_record.mass_assignment_sanitizer = :strict
