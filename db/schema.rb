44769608984a19918ad22b83cb6051ee94aceff2
“Jen <mpjeno@gmail.com>
2014-10-08 11:55:50 -0500

    added lat & long to locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141007161035) do
+ActiveRecord::Schema.define(:version => 20141008165424) do
 
   create_table "locations", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(:version => 20141007161035) do
     t.string   "url"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "products", :force => true do |t|
