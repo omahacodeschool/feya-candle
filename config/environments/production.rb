e4620057054f0e3e127c0f536cbf838212139d73
Andrea Talbot <andrea.talbot1@gmail.com>
2014-10-08 09:53:34 -0500

    initial css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,13 @@ FeyaCandle::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
    config.action_mailer.raise_delivery_errors = true
-   config.action_mailer.default_url_options = { :host => "feya-candle.herokuapp.com" }
+   config.action_mailer.default_url_options = { 
+     :host => "feya-candle.herokuapp.com",
+     :protocol => "https"
+   }
+
+   
+
 
   # Enable threaded mode
   # config.threadsafe!
