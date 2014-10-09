f9878ae25ba7f1e3eeb17c14efab851ccd7b845b
“Jen <mpjeno@gmail.com>
2014-10-09 14:00:50 -0500

    form partials and index for Sarah

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,10 @@
 class Contact < ActiveRecord::Base
-  attr_accessible :company, :email, :message, :name, :phone
+  attr_accessible :company, :email, :message, :name, :phone, :category
   
   validates :name, presence: true
   validates :email, presence: true
   validates :phone, presence: true
   validates :message, presence: true
+  validates :category, presence: true
 
 end
