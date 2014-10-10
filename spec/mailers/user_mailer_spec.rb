b5a5c93635de44360f81ea63a27e1cfe6f91a85d
Andrea Talbot <andrea.talbot1@gmail.com>
2014-10-10 13:23:15 -0500

    rspec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,18 +1,18 @@
-require "rails_helper"
-
-RSpec.describe UserMailer, :type => :mailer do
-  describe "reset_password_email" do
-    let(:mail) { UserMailer.reset_password_email }
-
-    it "renders the headers" do
-      expect(mail.subject).to eq("Reset password email")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
-    end
-
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
-    end
-  end
-
-end
+# require "rails_helper"
+#
+# RSpec.describe UserMailer, :type => :mailer do
+#   describe "reset_password_email" do
+#     let(:user) { :name => 'Test Person', :email => 'test@test.com' }
+#     let(:mail) { UserMailer.reset_password_email(user) }
+#
+#     it "renders the header information" do
+#       expect(mail.subject).to eq("Your password has been reset.")
+#       expect(mail.to).to eq([user.email])
+#       expect(mail.from).to eq(["feyacandle@gmail.com"])
+#     end
+#
+#     it "renders the body" do
+#       expect(mail.body.encoded).to match("Hi")
+#     end
+#   end
+# end
