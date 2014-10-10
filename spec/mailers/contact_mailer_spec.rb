5227286b7f9358d5577894297c6024f8f1c70cac
“Jen <mpjeno@gmail.com>
2014-10-10 08:31:03 -0500

    attempting tests for mailers. failing miserably.

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -1,5 +1,25 @@
 require "rails_helper"
 
 RSpec.describe ContactMailer, :type => :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "wholesale_inquiry" do
+    
+    it 'renders the subject' do
+      contact = Contact.create(name: 'Jen', email: 'test@test.com', phone: '555.555.5555', message: 'Hi')
+      mail = ContactMailer.wholesale_inquiry(contact)
+      expect(mail.subject).to eql('Wholesale inquiry')
+    end
+    
+    it 'renders the receiver email' do
+      expect(mail.to).to eql([contact.email])
+    end
+    
+    it 'renders the sender email' do
+      expect(mail.from).to eql(['feyacandle@gmail.com'])
+    end
+    
+    it 'renders the message' do
+      expect(mail.message).to eql(['Hi'])
+    end
+    
+  end 
 end
